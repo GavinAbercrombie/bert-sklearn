dd551ec9ccbde4d5313517a7c9955520107e4e53
GavinAbercrombie <36740883+GavinAbercrombie@users.noreply.github.com>
2019-11-24 15:51:54 +0000

    Update model.py

--- bert_sklearn/model/model.py
+++ bert_sklearn/model/model.py
@@ -4,41 +4,22 @@ import torch.nn as nn
 from .pytorch_pretrained import BertModel
 from .pytorch_pretrained import BertPreTrainedModel
 
-def LinearBlock(H1, H2, p):
-    return nn.Sequential(
-        nn.Linear(H1, H2),
-        nn.BatchNorm1d(H2),
-        nn.ReLU(),
-        nn.Dropout(p))
-
 def CNN(D, n, H, K, p):
     """
-    CNN w batchnorm and dropout.
+    CNN.
 
     Parameters
     ----------
     D : int, size of input layer
-    n : int, number of hidden layers
     H : int, size of hidden layer
     K : int, size of output layer
-    p : float, dropout probability
     """
-
-    if n == 0:
-        print("Defaulting to linear classifier/regressor")
-        return nn.Linear(D, K)
-    else:
-        print("Using CNN with D=%d,H=%d,K=%d,n=%d"%(D, H, K, n))
-        layers = [nn.Conv1d(D, K, 2),
-                  nn.MaxPool1d(1),
-                  nn.Linear(H, K)]
-        return torch.nn.Sequential(*layers)
-        #layers = [nn.BatchNorm1d(D),
-        #         LinearBlock(D, H, p)]
-        #for _ in range(n-1):
-        #    layers.append(LinearBlock(H, H, p))
-        #layers.append(nn.Linear(H, K))
-        #return torch.nn.Sequential(*layers)
+    
+    print("Using CNN with D=%d,H=%d,K=%d,n=%d"%(D, H, K))
+    layers = [nn.Conv1d(D, K, 2),
+              nn.MaxPool1d(1),
+              nn.Linear(H, K)]
+    return torch.nn.Sequential(*layers)
 
 
 class BertPlusCNN(BertPreTrainedModel):
@@ -51,43 +32,16 @@ class BertPlusCNN(BertPreTrainedModel):
     ----------
     config : BertConfig
         stores configuration of BertModel
-
-    model_type : string
-         'text_classifier'
-
-    num_labels : int
-        For a classifier, this is the number of distinct classes.
-        For a regressor his will be 1.
-
-    num_mlp_layers : int
-        the number of mlp layers. If set to 0, then defualts
-        to the linear classifier/regresor in the original Google paper and code.
-
-    num_mlp_hiddens : int
-        the number of hidden neurons in each layer of the mlp.
     """
 
-    def __init__(self, config,
-                 model_type="text_classifier",
-                 num_labels=2,
-                 num_mlp_layers=2,
-                 num_mlp_hiddens=500):
+    def __init__(self, config):
 
         super(BertPlusCNN, self).__init__(config)
-        self.model_type = model_type
-        self.num_labels = num_labels
-        self.num_mlp_layers = num_mlp_layers
-        self.num_mlp_hiddens = num_mlp_hiddens
-
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        
         self.bert = BertModel(config)
         self.input_dim = config.hidden_size
 
-        self.cnn = CNN(D=self.input_dim,
-                       n=self.num_mlp_layers,
-                       H=self.num_mlp_hiddens,
-                       K=self.num_labels,
-                       p=config.hidden_dropout_prob)
+        self.cnn = CNN(D=self.input_dim)
 
         self.apply(self.init_bert_weights)
 
@@ -98,25 +52,11 @@ class BertPlusCNN(BertPreTrainedModel):
                                           input_mask,
                                           output_all_encoded_layers=False)
 
-        if self.model_type == "token_classifier":
-            output = hidden
-        else:
-            output = pooled_output
-            output = self.dropout(output)
-
         output = self.cnn(output)
 
         if labels is not None:
-            if self.model_type == "text_classifier":
-                loss_criterion = nn.CrossEntropyLoss(reduction='none')
-                loss = loss_criterion(output.view(-1, output.shape[-1]), labels.view(-1))
-            elif self.model_type == "text_regressor":
-                loss_criterion = nn.MSELoss(reduction='none')
-                output = torch.squeeze(output)
-                loss = loss_criterion(output, labels)
-            elif self.model_type == "token_classifier":
-                loss_criterion = nn.CrossEntropyLoss(reduction='none', ignore_index=-1)
-                loss = loss_criterion(output.view(-1, output.shape[-1]), labels.view(-1))
+            loss_criterion = nn.CrossEntropyLoss(reduction='none')
+            loss = loss_criterion(output.view(-1, output.shape[-1]), labels.view(-1))
             return loss, output
         else:
             return output
