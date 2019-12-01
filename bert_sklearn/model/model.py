3534fdc7d88015e4ec27f720da2d97b978c55608
GavinAbercrombie <36740883+GavinAbercrombie@users.noreply.github.com>
2019-12-01 21:10:16 +0000

    Update model.py

--- bert_sklearn/model/model.py
+++ bert_sklearn/model/model.py
@@ -26,7 +26,7 @@ def CNN(D, n, H, K, p):
     """
     
     print("Using mlp with D=%d,H=%d,K=%d,n=%d"%(D, H, K, n))
-    return torch.nn.Sequential(nn.Conv1d(D, K))
+    return torch.nn.Sequential(nn.Conv1d(D, K, 1))
     #layers = [nn.BatchNorm1d(D),
     #          LinearBlock(D, H, p)]
     #for _ in range(n-1):
